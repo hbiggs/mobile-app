cdcb0f9c90c5299e26efff317c8320b9be1c4705
Heather <heather@aahb.com.au>
2014-02-02 18:21:20 +1000

    Initial Commit

---
+++ jquerymobile/demos/jqm-search.php
@@ -0,0 +1,192 @@
+	<!-- TODO: This should become an external panel so we can add input to markup (unique ID) -->
+    <div data-role="panel" class="jqm-search-panel" data-position="right" data-display="overlay" data-theme="a">
+		<div class="jqm-search">
+			<ul class="jqm-list" data-filter-placeholder="Search demos..." data-filter-reveal="true">
+<li data-filtertext="demos homepage" data-icon="home"><a href="../../jquery-mobile-1.4">Home</a></li>
+<li data-filtertext="introduction overview getting started"><a href="../../jquery-mobile-1.4/intro" data-ajax="false">Introduction</a></li>
+<li data-filtertext="buttons button markup buttonmarkup method anchor link button element"><a href="../../jquery-mobile-1.4/button-markup" data-ajax="false">Buttons</a></li>
+<li data-filtertext="form button widget input button submit reset"><a href="../../jquery-mobile-1.4/button" data-ajax="false">Button widget</a></li>
+<li data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right" data-inset="false">
+	<h3>Checkboxradio widget</h3>
+	<ul>
+		<li data-filtertext="form checkboxradio widget checkbox input checkboxes controlgroups"><a href="../../jquery-mobile-1.4/checkboxradio-checkbox" data-ajax="false">Checkboxes</a></li>
+		<li data-filtertext="form checkboxradio widget radio input radio buttons controlgroups"><a href="../../jquery-mobile-1.4/checkboxradio-radio" data-ajax="false">Radio buttons</a></li>
+	</ul>
+</li>
+<li data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right" data-inset="false">
+	<h3>Collapsible (set) widget</h3>
+	<ul>
+		<li data-filtertext="collapsibles content formatting"><a href="../../jquery-mobile-1.4/collapsible" data-ajax="false">Collapsible</a></li>
+		<li data-filtertext="dynamic collapsible set accordion append expand"><a href="../../jquery-mobile-1.4/collapsible-dynamic" data-ajax="false">Dynamic collapsibles</a></li>
+		<li data-filtertext="accordions collapsible set widget content formatting grouped collapsibles"><a href="../../jquery-mobile-1.4/collapsibleset" data-ajax="false">Collapsible set</a></li>
+	</ul>
+</li>
+<li data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right" data-inset="false">
+	<h3>Controlgroup widget</h3>
+	<ul>
+		<li data-filtertext="controlgroups selectmenu checkboxradio input grouped buttons horizontal vertical"><a href="../../jquery-mobile-1.4/controlgroup" data-ajax="false">Controlgroup</a></li>
+		<li data-filtertext="dynamic controlgroup dynamically add buttons"><a href="../../jquery-mobile-1.4/controlgroup-dynamic" data-ajax="false">Dynamic controlgroups</a></li>
+	</ul>
+</li>
+<li data-filtertext="form datepicker widget date input"><a href="../../jquery-mobile-1.4/datepicker" data-ajax="false">Datepicker</a></li>
+<li data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right" data-inset="false">
+	<h3>Events</h3>
+	<ul>
+		<li data-filtertext="swipe to delete list items listviews swipe events"><a href="../../jquery-mobile-1.4/swipe-list" data-ajax="false">Swipe list items</a></li>
+		<li data-filtertext="swipe to navigate swipe page navigation swipe events"><a href="../../jquery-mobile-1.4/swipe-page" data-ajax="false">Swipe page navigation</a></li>
+	</ul>
+</li>
+<li data-filtertext="filterable filter elements sorting searching listview table"><a href="../../jquery-mobile-1.4/filterable" data-ajax="false">Filterable widget</a></li>
+<li data-filtertext="form flipswitch widget flip toggle switch binary select checkbox input"><a href="../../jquery-mobile-1.4/flipswitch" data-ajax="false">Flipswitch widget</a></li>
+<li data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right" data-inset="false">
+	<h3>Forms</h3>
+	<ul>
+		<li data-filtertext="forms text checkbox radio range button submit reset inputs selects textarea slider flipswitch label form elements"><a href="../../jquery-mobile-1.4/forms" data-ajax="false">Forms</a></li>
+		<li data-filtertext="form hide labels hidden accessible ui-hidden-accessible forms"><a href="../../jquery-mobile-1.4/forms-label-hidden-accessible" data-ajax="false">Hide labels</a></li>
+		<li data-filtertext="form field containers fieldcontain ui-field-contain forms"><a href="../../jquery-mobile-1.4/forms-field-contain" data-ajax="false">Field containers</a></li>
+		<li data-filtertext="forms disabled form elements"><a href="../../jquery-mobile-1.4/forms-disabled" data-ajax="false">Forms disabled</a></li>
+		<li data-filtertext="forms gallery examples overview forms text checkbox radio range button submit reset inputs selects textarea slider flipswitch label form elements"><a href="../../jquery-mobile-1.4/forms-gallery" data-ajax="false">Forms gallery</a></li>
+	</ul>
+</li>
+<li data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right" data-inset="false">
+	<h3>Grids</h3>
+	<ul>
+		<li data-filtertext="grids columns blocks content formatting rwd responsive css framework"><a href="../../jquery-mobile-1.4/grids" data-ajax="false">Grids</a></li>
+		<li data-filtertext="buttons in grids css framework"><a href="../../jquery-mobile-1.4/grids-buttons" data-ajax="false">Buttons in grids</a></li>
+		<li data-filtertext="custom responsive grids rwd css framework"><a href="../../jquery-mobile-1.4/grids-custom-responsive" data-ajax="false">Custom responsive grids</a></li>
+	</ul>
+</li>
+<li data-filtertext="blocks content formatting sections heading"><a href="../../jquery-mobile-1.4/body-bar-classes" data-ajax="false">Grouping and dividing content</a></li>
+<li data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right" data-inset="false">
+	<h3>Icons</h3>
+	<ul>
+		<li data-filtertext="button icons svg disc alt custom icon position"><a href="../../jquery-mobile-1.4/icons" data-ajax="false">Icons</a></li>
+		<li data-filtertext=""><a href="../../jquery-mobile-1.4/icons-grunticon" data-ajax="false">Grunticon loader</a></li>
+	</ul>
+</li>
+<li data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right" data-inset="false">
+	<h3>Listview widget</h3>
+	<ul>
+		<li data-filtertext="listview widget thumbnails icons nested split button collapsible ul ol"><a href="../../jquery-mobile-1.4/listview" data-ajax="false">Listview</a></li>
+		<li data-filtertext="autocomplete filterable reveal listview filtertextbeforefilter placeholder"><a href="../../jquery-mobile-1.4/listview-autocomplete" data-ajax="false">Listview autocomplete</a></li>
+		<li data-filtertext="autocomplete filterable reveal listview remote data filtertextbeforefilter placeholder"><a href="../../jquery-mobile-1.4/listview-autocomplete-remote" data-ajax="false">Listview autocomplete remote data</a></li>
+		<li data-filtertext="autodividers anchor jump scroll linkbars listview lists ul ol"><a href="../../jquery-mobile-1.4/listview-autodividers-linkbar" data-ajax="false">Listview autodividers linkbar</a></li>
+		<li data-filtertext="listview autodividers selector autodividersselector lists ul ol"><a href="../../jquery-mobile-1.4/listview-autodividers-selector" data-ajax="false">Listview autodividers selector</a></li>
+		<li data-filtertext="listview nested list items"><a href="../../jquery-mobile-1.4/listview-nested-lists" data-ajax="false">Listview Nested Listviews</a></li>
+		<li data-filtertext="listview collapsible list items flat"><a href="../../jquery-mobile-1.4/listview-collapsible-item-flat" data-ajax="false">Listview collapsible list items (flat)</a></li>
+		<li data-filtertext="listview collapsible list indented"><a href="../../jquery-mobile-1.4/listview-collapsible-item-indented" data-ajax="false">Listview collapsible list items (indented)</a></li>
+		<li data-filtertext="grid listview responsive grids responsive listviews lists ul"><a href="../../jquery-mobile-1.4/listview-grid" data-ajax="false">Listview responsive grid</a></li>
+	</ul>
+</li>
+<li data-filtertext="loader widget page loading navigation overlay spinner"><a href="../../jquery-mobile-1.4/loader" data-ajax="false">Loader widget</a></li>
+<li data-filtertext="navbar widget navmenu toolbars header footer"><a href="../../jquery-mobile-1.4/navbar" data-ajax="false">Navbar widget</a></li>
+<li data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right" data-inset="false">
+	<h3>Navigation</h3>
+	<ul>
+		<li data-filtertext="ajax navigation navigate widget history event method"><a href="../../jquery-mobile-1.4/navigation" data-ajax="false">Navigation</a></li>
+		<li data-filtertext="linking pages page links navigation ajax prefetch cache"><a href="../../jquery-mobile-1.4/navigation-linking-pages" data-ajax="false">Linking pages</a></li>
+		<li data-filtertext="php redirect server redirection server-side navigation"><a href="../../jquery-mobile-1.4/navigation-php-redirect" data-ajax="false">PHP redirect demo</a></li>
+	</ul>
+</li>
+<li data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right" data-inset="false">
+	<h3>Pages</h3>
+	<ul>
+		<li data-filtertext="pages page widget ajax navigation"><a href="../../jquery-mobile-1.4/pages" data-ajax="false">Pages</a></li>
+		<li data-filtertext="single page"><a href="../../jquery-mobile-1.4/pages-single-page" data-ajax="false">Single page</a></li>
+		<li data-filtertext="multipage multi-page page"><a href="../../jquery-mobile-1.4/pages-multi-page" data-ajax="false">Multi-page template</a></li>
+		<li data-filtertext="dialog page widget modal popup"><a href="../../jquery-mobile-1.4/pages-dialog" data-ajax="false">Dialog page</a></li>
+	</ul>
+</li>
+<li data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right" data-inset="false">
+	<h3>Panel widget</h3>
+	<ul>
+		<li data-filtertext="panel widget sliding panels reveal push overlay responsive"><a href="../../jquery-mobile-1.4/panel" data-ajax="false">Panel</a></li>
+		<li data-filtertext=""><a href="../../jquery-mobile-1.4/panel-external" data-ajax="false">External panels</a></li>
+		<li data-filtertext="panel "><a href="../../jquery-mobile-1.4/panel-fixed" data-ajax="false">Fixed panels</a></li>
+		<li data-filtertext="panel slide panels sliding panels shadow rwd responsive breakpoint"><a href="../../jquery-mobile-1.4/panel-responsive" data-ajax="false">Panels responsive</a></li>
+		<li data-filtertext="panel custom style custom panel width reveal shadow listview panel styling page background wrapper"><a href="../../jquery-mobile-1.4/panel-styling" data-ajax="false">Custom panel style</a></li>
+		<li data-filtertext="panel open on swipe"><a href="../../jquery-mobile-1.4/panel-swipe-open" data-ajax="false">Panel open on swipe</a></li>
+		<li data-filtertext="panels outside page internal external toolbars"><a href="../../jquery-mobile-1.4/panel-external-internal" data-ajax="false">Panel external and internal</a></li>
+	</ul>
+</li>
+<li data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right" data-inset="false">
+	<h3>Popup widget</h3>
+	<ul>
+		<li data-filtertext="popup widget popups dialog modal transition tooltip lightbox form overlay screen flip pop fade transition"><a href="../../jquery-mobile-1.4/popup" data-ajax="false">Popup</a></li>
+		<li data-filtertext="popup alignment position"><a href="../../jquery-mobile-1.4/popup-alignment" data-ajax="false">Popup alignment</a></li>
+		<li data-filtertext="popup arrow size popups popover"><a href="../../jquery-mobile-1.4/popup-arrow-size" data-ajax="false">Popup arrow size</a></li>
+		<li data-filtertext="dynamic popups popup images lightbox"><a href="../../jquery-mobile-1.4/popup-dynamic" data-ajax="false">Dynamic popups</a></li>
+		<li data-filtertext="popups with iframes scaling"><a href="../../jquery-mobile-1.4/popup-iframe" data-ajax="false">Popups with iframes</a></li>
+		<li data-filtertext="popup image scaling"><a href="../../jquery-mobile-1.4/popup-image-scaling" data-ajax="false">Popup image scaling</a></li>
+		<li data-filtertext="external popup outside multi-page"><a href="../../jquery-mobile-1.4/popup-outside-multipage" data-ajax="false">Popup outside multi-page</a></li>
+	</ul>
+</li>
+<li data-filtertext="form rangeslider widget dual sliders dual handle sliders range input"><a href="../../jquery-mobile-1.4/rangeslider" data-ajax="false">Rangeslider widget</a></li>
+<li data-filtertext="responsive web design rwd adaptive progressive enhancement PE accessible mobile breakpoints media query media queries"><a href="../../jquery-mobile-1.4/rwd" data-ajax="false">Responsive Web Design</a></li>
+<li data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right" data-inset="false">
+	<h3>Selectmenu widget</h3>
+	<ul>
+		<li data-filtertext="form selectmenu widget select input custom select menu selects"><a href="../../jquery-mobile-1.4/selectmenu" data-ajax="false">Selectmenu</a></li>
+		<li data-filtertext="form custom select menu selectmenu widget custom menu option optgroup multiple selects"><a href="../../jquery-mobile-1.4/selectmenu-custom" data-ajax="false">Custom select menu</a></li>
+		<li data-filtertext="filterable select filter popup dialog"><a href="../../jquery-mobile-1.4/selectmenu-custom-filter" data-ajax="false">Custom select menu with filter</a></li>
+	</ul>
+</li>
+<li data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right" data-inset="false">
+	<h3>Slider widget</h3>
+	<ul>
+		<li data-filtertext="form slider widget range input single sliders"><a href="../../jquery-mobile-1.4/slider" data-ajax="false">Slider</a></li>
+		<li data-filtertext="form slider widget flipswitch slider binary select flip toggle switch"><a href="../../jquery-mobile-1.4/slider-flipswitch" data-ajax="false">Slider flip toggle switch</a></li>
+		<li data-filtertext="form slider tooltip handle value input range sliders"><a href="../../jquery-mobile-1.4/slider-tooltip" data-ajax="false">Slider tooltip</a></li>
+	</ul>
+</li>
+<li data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right" data-inset="false">
+	<h3>Table widget</h3>
+	<ul>
+		<li data-filtertext="table widget reflow column toggle th td responsive tables rwd hide show tabular"><a href="../../jquery-mobile-1.4/table-column-toggle" data-ajax="false">Table Column Toggle</a></li>
+		<li data-filtertext="table column toggle phone comparison demo"><a href="../../jquery-mobile-1.4/table-column-toggle-example" data-ajax="false">Table Column Toggle demo</a></li>
+		<li data-filtertext="responsive tables table column toggle heading groups rwd breakpoint"><a href="../../jquery-mobile-1.4/table-column-toggle-heading-groups" data-ajax="false">Table Column Toggle heading groups</a></li>
+		<li data-filtertext="responsive tables table column toggle hide rwd breakpoint customization options"><a href="../../jquery-mobile-1.4/table-column-toggle-options" data-ajax="false">Table Column Toggle options</a></li>
+		<li data-filtertext="table reflow th td responsive rwd columns tabular"><a href="../../jquery-mobile-1.4/table-reflow" data-ajax="false">Table Reflow</a></li>
+		<li data-filtertext="responsive tables table reflow heading groups rwd breakpoint"><a href="../../jquery-mobile-1.4/table-reflow-heading-groups" data-ajax="false">Table Reflow heading groups</a></li>
+		<li data-filtertext="responsive tables table reflow stripes strokes table style"><a href="../../jquery-mobile-1.4/table-reflow-stripes-strokes" data-ajax="false">Table Reflow stripes and strokes</a></li>
+		<li data-filtertext="responsive tables table reflow stack custom styles"><a href="../../jquery-mobile-1.4/table-reflow-styling" data-ajax="false">Table Reflow custom styles</a></li>
+	</ul>
+</li>
+<li data-filtertext="ui tabs widget"><a href="../../jquery-mobile-1.4/tabs" data-ajax="false">Tabs widget</a></li>
+<li data-filtertext="form textinput widget text input textarea number date time tel email file color password"><a href="../../jquery-mobile-1.4/textinput" data-ajax="false">Textinput widget</a></li>
+<li data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right" data-inset="false">
+	<h3>Theming</h3>
+	<ul>
+		<li data-filtertext="default theme swatches theming style css"><a href="../../jquery-mobile-1.4/theme-default" data-ajax="false">Default theme</a></li>
+		<li data-filtertext="classic theme old theme swatches theming style css"><a href="../../jquery-mobile-1.4/theme-classic" data-ajax="false">Classic theme</a></li>
+	</ul>
+</li>
+<li data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right" data-inset="false">
+	<h3>Toolbar widget</h3>
+	<ul>
+		<li data-filtertext="toolbar widget header footer toolbars fixed fullscreen external sections"><a href="../../jquery-mobile-1.4/toolbar" data-ajax="false">Toolbar</a></li>
+		<li data-filtertext="dynamic toolbars dynamically add toolbar header footer"><a href="../../jquery-mobile-1.4/toolbar-dynamic" data-ajax="false">Dynamic toolbars</a></li>
+		<li data-filtertext="external toolbars header footer"><a href="../../jquery-mobile-1.4/toolbar-external" data-ajax="false">External toolbars</a></li>
+		<li data-filtertext="fixed toolbars header footer"><a href="../../jquery-mobile-1.4/toolbar-fixed" data-ajax="false">Fixed toolbars</a></li>
+		<li data-filtertext="fixed fullscreen toolbars header footer"><a href="../../jquery-mobile-1.4/toolbar-fixed-fullscreen" data-ajax="false">Fullscreen toolbars</a></li>
+		<li data-filtertext="external fixed toolbars header footer"><a href="../../jquery-mobile-1.4/toolbar-fixed-external" data-ajax="false">Fixed external toolbars</a></li>
+		<li data-filtertext="external persistent toolbars header footer navbar navmenu"><a href="../../jquery-mobile-1.4/toolbar-fixed-persistent" data-ajax="false">Persistent toolbars</a></li>
+		<li data-filtertext="external ajax optimized toolbars persistent toolbars header footer navbar"><a href="../../jquery-mobile-1.4/toolbar-fixed-persistent-optimized" data-ajax="false">AJAX optimized toolbars</a></li>
+		<li data-filtertext="form in toolbars header footer"><a href="../../jquery-mobile-1.4/toolbar-fixed-forms" data-ajax="false">Form in toolbar</a></li>
+	</ul>
+</li>
+<li data-filtertext="page transitions animated pages popup navigation flip slide fade pop"><a href="../../jquery-mobile-1.4/transitions" data-ajax="false">Transitions</a></li>
+<li data-role="collapsible" data-collapsed-icon="carat-d" data-expanded-icon="carat-u" data-iconpos="right" data-inset="false">
+	<h3>3rd party API demos</h3>
+	<ul>
+		<li data-filtertext="backbone requirejs navigation router"><a href="../../jquery-mobile-1.4/backbone-requirejs" data-ajax="false">Backbone RequireJS</a></li>
+		<li data-filtertext="google maps geolocation demo"><a href="../../jquery-mobile-1.4/map-geolocation" data-ajax="false">Google Maps geolocation</a></li>
+		<li data-filtertext="google maps hybrid"><a href="../../jquery-mobile-1.4/map-list-toggle" data-ajax="false">Google Maps list toggle</a></li>
+	</ul>
+</li>
+
+
+
+			</ul>
+		</div>
+	</div><!-- /panel -->
